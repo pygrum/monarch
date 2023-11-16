2db38aa06278502849789f53a9f3cff5751bf141
pygrum <pygrum.sec@gmail.com>
2023-11-16 00:06:11 +0000

    interactive builder support

--- pkg/console/console.go
+++ pkg/console/console.go
@@ -2,6 +2,7 @@ package console
 
 import (
 	"github.com/pygrum/monarch/pkg/commands"
+	"github.com/pygrum/monarch/pkg/commands/build"
 	"github.com/pygrum/monarch/pkg/db"
 	"github.com/reeflective/console"
 )
@@ -10,20 +11,29 @@ type monarchServer struct {
 	App *console.Console
 }
 
-var Server *monarchServer
+var server *monarchServer
 
 func init() {
-	Server = &monarchServer{
+	server = &monarchServer{
 		App: console.New("monarch"),
 	}
 	db.Initialize()
+
+}
+
+// BuildMode switches the console to build mode. The name parameter is the name of the agent to be built.
+// Only creating a new menu each time because I want it to be named
+func BuildMode(name string) {
+	buildMenu := server.App.NewMenu(name)
+	buildMenu.AddCommand(build.ConsoleCommands()...)
+	server.App.SwitchMenu(name)
 }
 
 func Run() error {
 	// TODO:Start monarch console
-	srvMenu := Server.App.ActiveMenu()
+	srvMenu := server.App.ActiveMenu()
 	srvMenu.AddCommand(commands.ConsoleCommands()...)
-
 	srvMenu.CompletionOptions.HiddenDefaultCmd = true
-	return Server.App.Start()
+
+	return server.App.Start()
 }
