56e672df401dd9af1a4240a2ed6b7bcf3bc7910b
pygrum <pygrum.sec@gmail.com>
2023-11-22 22:47:41 +0000

    new use command to switch to active session and use its commands

--- pkg/commands/commands.go
+++ pkg/commands/commands.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
+	"github.com/pygrum/monarch/pkg/console"
 	"github.com/pygrum/monarch/pkg/log"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 var cLogger log.Logger
@@ -33,18 +35,13 @@ func ConsoleCommands() []*cobra.Command {
 		},
 	}
 
-	var buildersShowTranslator bool
 	cmdBuilders := &cobra.Command{
-		Use:   "builders",
+		Use:   "builders [names...]",
 		Short: "list installed builders",
-		Args:  cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
-			buildersCmd(buildersShowTranslator)
+			buildersCmd(args)
 		},
 	}
-	cmdBuilders.Flags().BoolVarP(&buildersShowTranslator, "show-translator", "t", false,
-		"show corresponding translator IDs")
-
 	cmdAgents := &cobra.Command{
 		Use:   "agents [names...]",
 		Short: "list compiled agents",
@@ -53,6 +50,43 @@ func ConsoleCommands() []*cobra.Command {
 		},
 	}
 
-	root = append(root, cmdAgents, cmdBuilders, cmdBuild, cmdExit)
+	cmdSessions := &cobra.Command{
+		Use:   "sessions [ids...]",
+		Short: "list established agent connections",
+		Run: func(cmd *cobra.Command, args []string) {
+			sessionsCmd(args)
+		},
+	}
+
+	cmdUse := &cobra.Command{
+		Use:   "use [id]",
+		Short: "initiate an interactive session with an agent",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			id, err := strconv.Atoi(args[0])
+			if err != nil {
+				cLogger.Error("%s is not a valid session ID", args[0])
+				return
+			}
+			useCmd(id)
+		},
+	}
+	root = append(root, cmdSessions, cmdUse, cmdAgents, cmdBuilders, cmdBuild, cmdExit)
 	return root
+}
+
+// exits any named menus spawned by any commands
+func cmdExit(short string) *cobra.Command {
+	if len(short) == 0 {
+		short = "exit the interactive menu"
+	}
+	cmd := &cobra.Command{
+		Use:   "exit",
+		Short: short,
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			console.MainMenu()
+		},
+	}
+	return cmd
 }
--- pkg/commands/use.go
+++ pkg/commands/use.go
@@ -1,1 +1,92 @@
 package commands
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/pygrum/monarch/pkg/console"
+	"github.com/pygrum/monarch/pkg/db"
+	"github.com/pygrum/monarch/pkg/docker"
+	"github.com/pygrum/monarch/pkg/handlers/xhttp"
+	"github.com/pygrum/monarch/pkg/rpcpb"
+	"github.com/pygrum/monarch/pkg/transport"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"os"
+	"strconv"
+)
+
+func useCmd(id int) {
+	ctx := context.Background()
+	sessionInfo := xhttp.Handler.SessionByID(id)
+	if sessionInfo == nil {
+		cLogger.Error("session '%d' not found", id)
+		return
+	}
+	builder := &db.Builder{}
+	if err := db.FindOneConditional("builder_id = ?", sessionInfo.Agent.Builder, builder); err != nil ||
+		len(builder.BuilderID) == 0 {
+		cLogger.Error("failed to acquire commands: %v", err)
+		return
+	}
+	rpc, err := docker.RPCAddresses(docker.Cli, ctx, builder.BuilderID)
+	if err != nil {
+		cLogger.Error("failed to acquire commands: %v", err)
+		return
+	}
+	conn, err := grpc.Dial(rpc, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		cLogger.Error("RPC connection failed: %v", err)
+		return
+	}
+	client := rpcpb.NewBuilderClient(conn)
+	descriptions, err := client.GetCommands(ctx, &rpcpb.DescriptionsRequest{})
+	if err != nil {
+		cLogger.Error("failed to acquire command descriptions (rpc): %v", err)
+		return
+	}
+	var cmds []*cobra.Command
+	for _, description := range descriptions.Descriptions {
+		args := cobra.NoArgs
+		if description.NumArgs > 0 {
+			args = cobra.ExactArgs(int(description.NumArgs))
+		} else if description.NumArgs < 0 {
+			args = cobra.ArbitraryArgs
+		}
+		cmd := &cobra.Command{
+			Use:   description.Usage,
+			Short: description.DescriptionShort,
+			Long:  description.DescriptionLong,
+			Args:  args,
+			Run: func(cmd *cobra.Command, args []string) {
+				byteArgs := make([][]byte, len(args))
+				for i, arg := range args {
+					data := []byte(arg)
+					// Refers to a file if prefixed and suffixed with @
+					if arg[0] == '@' && arg[len(arg)-1] == '@' {
+						filename := arg[1 : len(arg)-1]
+						bytes, err := os.ReadFile(filename)
+						if err != nil {
+							cLogger.Error("failed to read file %s", filename)
+							return
+						}
+						data = bytes
+					}
+					byteArgs[i] = data
+				}
+				req := &transport.GenericHTTPRequest{
+					AgentID:   sessionInfo.Agent.AgentID,
+					RequestID: uuid.New().String(),
+					Opcode:    description.Opcode,
+					Args:      byteArgs,
+				}
+				if err = xhttp.Handler.QueueRequest(sessionInfo.ID, req); err != nil {
+					cLogger.Error("failed to queue request: %v", err)
+				}
+			},
+		}
+		cmds = append(cmds, cmd)
+	}
+	cmds = append(cmds, cmdExit(""))
+	console.NamedMenu(strconv.Itoa(sessionInfo.ID), cmds)
+}
