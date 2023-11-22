917f8d739d7d67cf2829661cb368fbd7ae6aa29e
pygrum <pygrum.sec@gmail.com>
2023-11-22 22:50:24 +0000

    requestID in response changed to string

--- pkg/transport/transport.go
+++ pkg/transport/transport.go
@@ -1,22 +1,30 @@
 package transport
 
+import "github.com/pygrum/monarch/pkg/rpcpb"
+
+const (
+	DestFile int32 = iota
+	DestStdout
+)
+
 // GenericHTTPRequest is the structure sent by the C2 when an operator requests for an agent to perform a task.
 type GenericHTTPRequest struct {
 	AgentID   string
-	RequestID int32
+	RequestID string
 	Opcode    int32
 	Args      [][]byte
 }
 
 type ResponseDetail struct {
-	Dest int32
-	Data []byte
+	Dest int32  // Where to send response to (file, stdout)
+	Name string // Name of file to save, if applicable
+	Data []byte // file or output data
 }
 
 // GenericHTTPResponse is the structure received from an agent after a task is performed
 type GenericHTTPResponse struct {
 	AgentID   string
-	RequestID int32
-	Status    int32
+	RequestID string
+	Status    rpcpb.Status
 	Responses []ResponseDetail
 }
