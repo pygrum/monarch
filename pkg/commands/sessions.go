edc67ea2d9d9167db84cb46373f8bff95d5151ea
pygrum <pygrum.sec@gmail.com>
2023-11-22 22:50:38 +0000

    new command to list sessions

--- pkg/commands/sessions.go
+++ pkg/commands/sessions.go
@@ -1,1 +1,34 @@
 package commands
+
+import (
+	"fmt"
+	"github.com/pygrum/monarch/pkg/handlers/xhttp"
+	"strconv"
+	"time"
+)
+
+func sessionsCmd(sessionIDs []string) {
+	var sessIDs = make([]int, len(sessionIDs))
+	for i, id := range sessionIDs {
+		intID, err := strconv.Atoi(id)
+		if err != nil {
+			cLogger.Error("'%v' is not a valid session ID", id)
+			return
+		}
+		sessIDs[i] = intID
+	}
+	sessions := xhttp.Handler.Sessions(sessIDs)
+	header := "ID\tAGENT ID\tAGENT NAME\tQUEUE SIZE\tLAST ACTIVE\t"
+	_, _ = fmt.Fprintln(w, header)
+	for _, session := range sessions {
+		line := fmt.Sprintf("%v\t%v\t%v\t%v\t%v\t",
+			session.ID,
+			session.Agent.AgentID,
+			session.Agent.Name,
+			session.Queue.Size(),
+			session.LastActive.Format(time.DateTime),
+		)
+		_, _ = fmt.Fprintln(w, line)
+	}
+	_ = w.Flush()
+}
