aa1553015d19c4c5b22c8a9b028f551d16207355
pygrum <pygrum.sec@gmail.com>
2023-11-24 21:20:19 +0000

    removing repo if install fails

--- pkg/commands/install.go
+++ pkg/commands/install.go
@@ -1,10 +1,21 @@
 package commands
 
-import "github.com/pygrum/monarch/pkg/install"
+import (
+	"github.com/pygrum/monarch/pkg/config"
+	"github.com/pygrum/monarch/pkg/install"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func installCmd(repoUrl string, useCreds bool) {
 	if err := install.NewRepo(repoUrl, useCreds); err != nil {
 		cLogger.Error("failed to install %s: %v", repoUrl, err)
+		clonePath := filepath.Join(config.MainConfig.InstallDir, strings.TrimSuffix(filepath.Base(repoUrl),
+			filepath.Ext(filepath.Base(repoUrl))))
+		if err = os.RemoveAll(clonePath); err != nil {
+			cLogger.Error("failed to remove %s: %v. must be manually removed", clonePath, err)
+		}
 		return
 	}
 	cLogger.Success("installation successful")
