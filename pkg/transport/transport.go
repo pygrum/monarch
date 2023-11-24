b4c444923aa7568be631e514f36f4f3acc0afbc2
pygrum <pygrum.sec@gmail.com>
2023-11-24 21:19:32 +0000

    labelling of json fields to capture fields properly

--- pkg/transport/transport.go
+++ pkg/transport/transport.go
@@ -9,22 +9,22 @@ const (
 
 // GenericHTTPRequest is the structure sent by the C2 when an operator requests for an agent to perform a task.
 type GenericHTTPRequest struct {
-	AgentID   string
-	RequestID string
-	Opcode    int32
-	Args      [][]byte
+	AgentID   string   `json:"agent_id"`
+	RequestID string   `json:"request_id"`
+	Opcode    int32    `json:"opcode"`
+	Args      [][]byte `json:"args"`
 }
 
 type ResponseDetail struct {
-	Dest int32  // Where to send response to (file, stdout)
-	Name string // Name of file to save, if applicable
-	Data []byte // file or output data
+	Dest int32  `json:"dest"` // Where to send response to (file, stdout)
+	Name string `json:"name"` // Name of file to save, if applicable
+	Data []byte `json:"data"` // file or output data
 }
 
 // GenericHTTPResponse is the structure received from an agent after a task is performed
 type GenericHTTPResponse struct {
-	AgentID   string
-	RequestID string
-	Status    rpcpb.Status
-	Responses []ResponseDetail
+	AgentID   string           `json:"agent_id"`
+	RequestID string           `json:"request_id"`
+	Status    rpcpb.Status     `json:"status"`
+	Responses []ResponseDetail `json:"responses"`
 }
