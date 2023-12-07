4eb18251f7e02bfc18c789637a5288ec7cef93e6
pygrum <pygrum.sec@gmail.com>
2023-12-07 19:07:32 +0000

    pass non-nil clientpb.Empty to requests

--- pkg/commands/http.go
+++ pkg/commands/http.go
@@ -3,28 +3,29 @@ package commands
 import (
 	"github.com/pygrum/monarch/pkg/console"
 	"github.com/pygrum/monarch/pkg/log"
+	"github.com/pygrum/monarch/pkg/protobuf/clientpb"
 )
 
 // httpCmd starts an HTTP listener for incoming connections, whether it be from c2 profiles or agents directly
 func httpCmd(stop bool) {
 	if stop {
-		if _, err := console.Rpc.HttpClose(ctx, nil); err != nil {
+		if _, err := console.Rpc.HttpClose(ctx, &clientpb.Empty{}); err != nil {
 			cLogger.Error("%v", err)
 			return
 		}
 	}
-	notif, _ := console.Rpc.HttpOpen(ctx, nil)
+	notif, _ := console.Rpc.HttpOpen(ctx, &clientpb.Empty{})
 	log.NumericalLevel(cLogger, uint16(notif.LogLevel), notif.Msg)
 }
 
 // same as httpCmd but starts an HTTPS listener
 func httpsCmd(stop bool) {
 	if stop {
-		if _, err := console.Rpc.HttpsClose(ctx, nil); err != nil {
+		if _, err := console.Rpc.HttpsClose(ctx, &clientpb.Empty{}); err != nil {
 			cLogger.Error("%v", err)
 			return
 		}
 	}
-	notif, _ := console.Rpc.HttpsOpen(ctx, nil)
+	notif, _ := console.Rpc.HttpsOpen(ctx, &clientpb.Empty{})
 	log.NumericalLevel(cLogger, uint16(notif.LogLevel), notif.Msg)
 }
