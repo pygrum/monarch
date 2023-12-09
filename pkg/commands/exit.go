d2e2da51eca535932c540bd074ab61735c56123c
pygrum <pygrum.sec@gmail.com>
2023-12-09 01:32:05 +0000

    exit without confirmation

--- pkg/commands/exit.go
+++ pkg/commands/exit.go
@@ -2,24 +2,11 @@ package commands
 
 import (
 	"os"
-
-	"github.com/AlecAivazis/survey/v2"
 )
 
-func exitCmd(yes bool) {
-	if yes {
-		cLogger.Info("Goodbye!")
-		os.Exit(0)
-	}
-	y := false
-	prompt := &survey.Confirm{
-		Message: "are you sure you want to quit? All listeners and sessions will be closed",
-	}
-	_ = survey.AskOne(prompt, &y)
-	if y {
-		cLogger.Info("Goodbye!")
-		_ = cLogger.Close()
+func exitCmd() {
+	cLogger.Info("Goodbye!")
+	_ = cLogger.Close()
 
-		os.Exit(0)
-	}
+	os.Exit(0)
 }
