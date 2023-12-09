3f256b059762fdbd15db928671f3e8bac3f3e0f3
pygrum <pygrum.sec@gmail.com>
2023-12-09 20:24:10 +0000

    crypto helpers

--- pkg/crypto/aes.go
+++ pkg/crypto/aes.go
@@ -1,1 +1,52 @@
 package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func EncryptAES(key []byte, plaintext string) (string, error) {
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return "", err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = rand.Read(nonce)
+	if err != nil {
+		return "", err
+	}
+	ciphertext := gcm.Seal(nonce, nonce, []byte(plaintext), nil)
+
+	return hex.EncodeToString(ciphertext), nil
+}
+
+func DecryptAES(key []byte, ciphertext string) (string, error) {
+	ct, err := hex.DecodeString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+	ciphertext = string(ct)
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		panic(err)
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+
+	plaintext, err := gcm.Open(nil, []byte(nonce), []byte(ciphertext), nil)
+	if err != nil {
+		return "", err
+	}
+	return string(plaintext), nil
+}
